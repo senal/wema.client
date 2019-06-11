d5a8e664822df5f2532102b27716730a3ad24d44
Ranga Fernando <ranga.fernando@gmail.com>
2019-06-11 22:06:09 +1000

    Updates

---
+++ precache-manifest.a4e4c310625b4df360820b9797def697.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "13cd04c5b8982cb388745156a43e6dd3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e0f02fd8832c1342e052",
+    "url": "/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "e0f02fd8832c1342e052",
+    "url": "/static/js/2.2e463654.chunk.js"
+  },
+  {
+    "revision": "58bfbd3c3fabd65bbfa4",
+    "url": "/static/js/main.016fab22.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
